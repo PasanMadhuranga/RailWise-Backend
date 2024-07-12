fb421b90e1d74bc4c9da2a11c65434432fda5aa2
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-12 21:33:16 +0530

    create middleware to verify token

---
+++ utils/middleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import ExpressError from "./ExpressError.utils.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    throw new ExpressError("Unauthorized", 401);
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    req.userId = decoded.id; // decoded is the payload that is provided when creating the token, in this case, the user id
+    next();
+  });
+};
