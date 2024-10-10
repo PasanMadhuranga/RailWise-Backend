ddf7e03818f26eca5bd042128d813b368a982a72
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-10-11 02:41:04 +0530

    updated HOST

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 
 // Update this part to listen on your private IP
 const PORT = 3000;
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
