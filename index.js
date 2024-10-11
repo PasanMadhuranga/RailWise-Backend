323f562e27d3fc3210b7252b9baebc1772d91b43
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-10-11 14:08:38 +0530

    added 200 ok on root

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ app.use((err, req, res, next) => {
 const PORT = 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
 });
