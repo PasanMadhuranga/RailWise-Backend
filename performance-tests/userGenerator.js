941eede71c0ca637eafcb78946eb8e9ca10ff139
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-10-12 17:49:13 +0530

    added artillery testing

---
+++ performance-tests/userGenerator.js
@@ -0,0 +1,15 @@
+// performance-tests/userGenerator.js (CommonJS)
+const { faker } = require('@faker-js/faker');
+
+function generateUser() {
+  return {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: 'Password123!',
+    phone: faker.phone.number(),
+  };
+}
+
+module.exports = {
+  generateUser,
+};
